d780fcc508af6a20dd71474c382a296afe0e1ee6
Patrick Wu <wotingwu@live.com>
2018-08-19 21:00:08 +0800

    placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='grewritingpool',
+    version='0.0.1',
+    description='a package that returns gre writing pool in json format',
+    license='MIT',
+    packages=['grewritingpool'],
+    author='Patrick Wu',
+    author_email='wotingwu@live.com',
+    keywords=['gre','writing'],
+    url=''
+)
