3e6abcf9fe27a399eb27c289ea0506445e127826
Patrick J. Wu <wotingwu@live.com>
2018-08-31 21:55:21 +0800

    general completion

--- grewritingpool.py
+++ grewritingpool.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 import json
 import requests
 from bs4 import BeautifulSoup as bs
 from bs4 import element
 
-
-def fetch_pool():
-    base_req = requests.get("https://www.ets.org/gre/revised_general/prepare/analytical_writing/argument/pool")
+def fetch_pool(writing_type):
+    base_req = requests.get("https://www.ets.org/gre/revised_general/prepare/analytical_writing/"+writing_type+"/pool")
     base_req.encoding = 'utf-8'
     base_res = base_req.text
     base_soup = bs(base_res,'lxml')
@@ -29,9 +29,33 @@ def fetch_pool():
             status = 2
         elif status == 2 and item.name == "div":
             status = 3
-            tmp["instru"] = item.contents[0]
+            tmp["instru"] = item.contents[1].contents[0]
         elif status == 3 and item.name == "div":
             pool.append(tmp)
             tmp = {}
             status = 1
-    print(pool)
+    return pool
+
+def fetch_type(fetchtype, isSave):
+    try:
+        if fetchtype == ( "issue" or "argument" ):
+            return save_json(fetchtype, fetch_pool(fetchtype)) if isSave else fetch_pool(fetchtype)
+        elif fetchtype == "all":
+            return fetch_type("issue", isSave), fetch_type("argument", isSave)
+        else:
+            raise ValueError("Invalid Fetch Type "+fetchtype+".")
+    except ValueError as err:
+        print(err.args)
+
+def save_json(name, data):
+    with open(name+'.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def main(args):
+    if len(args) == 1:
+
+    file = fetch_pool()
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='grewritingpool',
-    version='0.0.1',
+    version='0.0.2',
     description='a package that returns gre writing pool in json format',
     license='MIT',
     packages=['grewritingpool'],
