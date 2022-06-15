92be19f14c3da536d7796038ce1b754e2da1ff00
Jinming Wu, Patrick <me@patrickwu.space>
2022-06-16 00:49:58 +0800

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='grewritingpool',
-    version='0.2.1',
+    version='0.2.2',
     description='Helper for GRE Writing Pool',
     long_description=long_description,
     long_description_content_type="text/markdown",
